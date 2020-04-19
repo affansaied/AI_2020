fdad784d926efdc94ecf4ca2aba117c9b1271fd7
MASB <affansaied@gmail.com>
2020-04-20 00:08:00 +0500

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,9 +1,9 @@
-var1 = "4"
-var2 = "8"
+var1 = "8"
+var2 = "4"
 
 print(int(var1)+int(var2))
 print(int(var1)-int(var2))
-print(int(var1)/int(var2))
+print(int(var1)*int(var2))
 print(int(var1)/int(var2))
 
 print("All Done")
